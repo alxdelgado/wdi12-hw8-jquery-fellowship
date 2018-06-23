364714372002ae48d79dbeee12ab74e2aa31313b
Alex Delgado <delgadoalex@icloud.com>
2018-06-23 17:08:32 -0500

    Chapter 1 - Made Middle Earth

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -48,18 +48,37 @@ const makeMiddleEarth = () => {
 
   // 1. create a section tag with an id of middle-earth
 
+  const $section = $('<section>')
+  $section.attr('id', 'middle-earth')
+  console.log($section); 
+
   // 2. append the section to the body of the DOM.
+
+  $('body').append($section)
 
   // 3. use a for loop to iterate over the lands array that does the following:
 
+  for (let i = 0; i < lands.length; i++){
+   
+
+
   //   3a. creates an article tag (there should be one for each land when the loop is done)
+    const $article = $('<article/>')
 
   //   3b. gives each land article an `id` tag of the corresponding land name
+    $article.attr('id', lands[i]);
 
   //   3c. includes an h1 with the name of the land inside each land article
+    const $h1 = $('h1'); 
+    $h1.text(lands[i]);
+    $article.append($h1);
 
   //   3d. appends each land to the middle-earth section
+   
+    $section.append($article);
 
+
+  }
 };
 
 // COMMIT YOUR WORK
@@ -283,3 +302,6 @@ $(() => {
   $('#13').on('click', thereAndBackAgain);
 
 });
+
+
+
